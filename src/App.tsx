c6dfc76de3394d709cc28fbed1b5924673041362
Marwan Zogheib <mzogheib@gmail.com>
2022-03-12 09:56:24 +1100

    Move render code into Attempts

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 
 import AttemptInput from './components/AttemptInput';
-import Tile, { TileState } from './components/Tile';
-import Tiles from './components/Tiles';
 import Attempts from './components/Attempts';
 import ResultModal from './components/ResultModal';
 import AutoScrollToBottom from './components/AutoScrollToBottom';
 import { getRandomWord, TARGET_LENGTH } from './services/words';
 import AppHeader from './components/AppHeader';
-
-const POSITIONS = Array.from(Array(TARGET_LENGTH).keys());
 
 const Wrapper = styled.div`
   height: 100%;
@@ -21,14 +17,6 @@ const Main = styled.main`
   padding: 10px;
   overflow: auto;
 `;
-
-const getTileState = (target: string, attempt: string, position: number) => {
-  return target[position] === attempt[position]
-    ? TileState.Match
-    : target.includes(attempt[position])
-    ? TileState.Present
-    : TileState.Absent;
-};
 
 const App = () => {
   const [attempts, setAttempts] = useState<string[]>([]);
@@ -48,18 +36,6 @@ const App = () => {
     setAttempts(attempts.concat([attempt]));
   };
 
-  const renderAttempt = (attempt: string) => (
-    <Tiles>
-      {POSITIONS.map((position) => (
-        <Tile
-          key={position}
-          letter={attempt[position]}
-          state={getTileState(target, attempt, position)}
-        />
-      ))}
-    </Tiles>
-  );
-
   const lastAttempt = attempts.length
     ? attempts[attempts.length - 1]
     : undefined;
@@ -72,11 +48,7 @@ const App = () => {
 
         <Main>
           {!!attempts.length && (
-            <Attempts>
-              {attempts.map((attempt, i) => (
-                <div key={`${attempt}-${i}`}>{renderAttempt(attempt)}</div>
-              ))}
-            </Attempts>
+            <Attempts attempts={attempts} target={target} />
           )}
           {!didSucceed && (
             <AttemptInput onSubmit={handleSubmit} length={TARGET_LENGTH} />
--- src/components/Attempts.tsx
+++ src/components/Attempts.tsx
@@ -1,10 +1,49 @@
 import styled from '@emotion/styled';
 
-const Attempts = styled.div`
+import Tile, { TileState } from './Tile';
+import Tiles from './Tiles';
+
+const getTileState = (target: string, attempt: string, position: number) => {
+  return target[position] === attempt[position]
+    ? TileState.Match
+    : target.includes(attempt[position])
+    ? TileState.Present
+    : TileState.Absent;
+};
+
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-
   align-items: center;
 `;
+
+type Props = {
+  target: string;
+  attempts: string[];
+};
+
+const Attempts = ({ target, attempts }: Props) => {
+  const POSITIONS = Array.from(Array(target.length).keys());
+
+  const renderAttempt = (attempt: string) => (
+    <Tiles>
+      {POSITIONS.map((position) => (
+        <Tile
+          key={position}
+          letter={attempt[position]}
+          state={getTileState(target, attempt, position)}
+        />
+      ))}
+    </Tiles>
+  );
+
+  return (
+    <Wrapper>
+      {attempts.map((attempt, i) => (
+        <div key={`${attempt}-${i}`}>{renderAttempt(attempt)}</div>
+      ))}
+    </Wrapper>
+  );
+};
 
 export default Attempts;
