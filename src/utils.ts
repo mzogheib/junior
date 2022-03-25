312c6739db2ad90c20c17504486fccddb8736a7c
Marwan Zogheib <mzogheib@gmail.com>
2022-03-26 10:20:16 +1100

    Make a usePrevious hook

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,12 @@
+import { useEffect, useRef } from 'react';
+
+export const usePrevious = <T>(value: T): T | undefined => {
+  const ref = useRef<T>();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
 export const randomNumberBetween = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1) + min);
