7c3ae17cf383f25bda337b9adc0c617b3f7ebb1f
Marwan Zogheib <mzogheib@gmail.com>
2022-03-26 14:51:19 +1100

    Speed up loading time

--- src/services/words.ts
+++ src/services/words.ts
@@ -7,6 +7,6 @@ export const getRandomWord = (): Promise<string> => {
   const index = randomNumberBetween(0, WORDS_LENGTH - 1);
 
   return new Promise((resolve) =>
-    setTimeout(() => resolve(words[index].toUpperCase()), 1000)
+    setTimeout(() => resolve(words[index].toUpperCase()), 500)
   );
 };
