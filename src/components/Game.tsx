4718c98fc59186017eb17e2ad9489e806e010ae6
Marwan Zogheib <mzogheib@gmail.com>
2022-04-23 13:30:50 +1000

    Refactor

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -27,6 +27,14 @@ const gameConfig = {
   },
 };
 
+const checkDidSucceed = (attempts: string[], target: string) => {
+  const lastAttempt = attempts.length
+    ? attempts[attempts.length - 1]
+    : undefined;
+
+  return !!lastAttempt && lastAttempt === target;
+};
+
 type Props = {
   targetSegments: TargetSegments;
   gameMode: GameMode;
@@ -35,58 +43,49 @@ type Props = {
 const Game = ({ targetSegments, gameMode }: Props) => {
   const [attempts, setAttempts] = useState<string[]>([]);
   const [error, setError] = useState("");
-  const target = stringifyTargetSegments(targetSegments);
 
-  const lastAttempt = attempts.length
-    ? attempts[attempts.length - 1]
-    : undefined;
-  const didSucceed = !!lastAttempt && lastAttempt === target;
+  const { tileSize, validate } = gameConfig[gameMode];
+  const target = stringifyTargetSegments(targetSegments);
+  const didSucceed = checkDidSucceed(attempts, target);
+
+  const handleValidate = (attemptSegments: TargetSegments) => {
+    const error = validate(attemptSegments);
+
+    if (error) {
+      setError(error);
+      return false;
+    }
+
+    return true;
+  };
 
   const handleSubmit = (attempt: string) => {
     setError("");
-
     setAttempts(attempts.concat([attempt]));
-  };
-
-  const { tileSize, validate } = gameConfig[gameMode];
-
-  const renderAttempts = () =>
-    !!attempts.length && (
-      <Attempts
-        attempts={attempts}
-        target={stringifyTargetSegments(targetSegments)}
-        readOnlyValues={READ_ONLY_CHARACTERS}
-        characterMap={CHARACTER_DISPLAY_MAP}
-        size={tileSize}
-      />
-    );
-
-  const renderInput = () => {
-    const handleValidate = (targetSegments: TargetSegments) => {
-      const error = validate(targetSegments);
-      if (error) {
-        setError(error);
-        return false;
-      }
-
-      return true;
-    };
-
-    return (
-      <TileInputForm
-        mode={gameMode}
-        size={tileSize}
-        targetSegments={targetSegments}
-        onSubmit={handleSubmit}
-        onValidate={handleValidate}
-      />
-    );
   };
 
   return (
     <>
-      {renderAttempts()}
-      {!didSucceed && renderInput()}
+      {!!attempts.length && (
+        <Attempts
+          attempts={attempts}
+          target={stringifyTargetSegments(targetSegments)}
+          readOnlyValues={READ_ONLY_CHARACTERS}
+          characterMap={CHARACTER_DISPLAY_MAP}
+          size={tileSize}
+        />
+      )}
+
+      {!didSucceed && (
+        <TileInputForm
+          mode={gameMode}
+          size={tileSize}
+          targetSegments={targetSegments}
+          onSubmit={handleSubmit}
+          onValidate={handleValidate}
+        />
+      )}
+
       {didSucceed && <SuccessMessage numAttempts={attempts.length} />}
 
       {error && (
