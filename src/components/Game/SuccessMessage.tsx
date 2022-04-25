033b9a938b536e5e2d40fb846875a0632ef877ea
Marwan Zogheib <mzogheib@gmail.com>
2022-04-25 10:26:51 +1000

    Format the duration output

--- src/components/Game/SuccessMessage.tsx
+++ src/components/Game/SuccessMessage.tsx
@@ -8,18 +8,32 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const makeDuration = (startedAt: number, finishedAt: number) => {
+  const durationInSeconds = Math.round((finishedAt - startedAt) / 1000);
+  const durationInMinutes = Math.floor(durationInSeconds / 60);
+  const leftOverSeconds = durationInSeconds % 60;
+
+  if (durationInMinutes > 9) {
+    return "and you took way too long!";
+  }
+
+  if (durationInMinutes === 0) {
+    return `in ${leftOverSeconds}s`;
+  }
+
+  return `in ${durationInMinutes}m and ${leftOverSeconds}s`;
+};
+
 const makeMessage = (attempts: Attempt[], gameConfig: GameConfig) => {
   const numAttempts = attempts.length;
   const lastAttempt = attempts[numAttempts - 1];
-  const finishedAt = lastAttempt.submittedAt;
-  const duration = finishedAt - gameConfig.startedAt;
-  const durationInSeconds = Math.round(duration / 1000).toLocaleString();
+  const duration = makeDuration(gameConfig.startedAt, lastAttempt.submittedAt);
 
   if (numAttempts === 1) {
-    return `Solved in ${durationInSeconds}s with 1 attempt`;
+    return `Solved with 1 attempt ${duration}`;
   }
 
-  return `Solved in ${durationInSeconds}s with ${numAttempts} attempts`;
+  return `Solved with ${numAttempts} attempts ${duration}`;
 };
 
 type Props = {
