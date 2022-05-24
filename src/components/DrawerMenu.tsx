2f10546cafbce546a6d2999c15a994863bc26dc3
Marwan Zogheib <mzogheib@gmail.com>
2022-05-24 14:42:52 +1000

    Remove auth toggle

--- src/components/DrawerMenu.tsx
+++ src/components/DrawerMenu.tsx
@@ -5,17 +5,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 import ThemeToggle from "components/Theme/ThemeToggle";
 import { spacing } from "components/Theme/utils";
-import AuthToggle from "components/Auth/AuthToggle";
 import UserWelcome from "components/UserWelcome";
 import NewGameButton from "components/Game/NewGame/NewGameButton";
 
 const Wrapper = styled.div`
   padding: ${spacing(2)};
   max-width: 230px;
-`;
-
-const Item = styled.div`
-  margin-top: ${spacing(1)};
 `;
 
 type Props = {
@@ -32,9 +27,6 @@ const DrawerMenu = ({ isOpen, onClose, onClickNewGame }: Props) => {
     <Drawer anchor="right" open={isOpen} onClose={onClose}>
       <Wrapper>
         <UserWelcome firstName={firstName} />
-        <Item>
-          <AuthToggle />
-        </Item>
       </Wrapper>
       <Divider />
       <Wrapper>
