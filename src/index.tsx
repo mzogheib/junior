08b802da1388100bcec5613237fe69435842332d
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-06-22 11:54:41 +1000

    Add PWA support

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import * as serviceWorkerRegistration from "serviceWorkerRegistration";
+
 import App from "App";
 import reportWebVitals from "reportWebVitals";
 import ThemeProvider from "components/Theme/ThemeProvider";
@@ -25,6 +27,11 @@ if (container) {
     </React.StrictMode>
   );
 }
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
