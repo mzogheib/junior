1b1f34a7d3c05b4a116d31aaedcf9fa143b26970
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-12-25 12:50:38 +1100

    Create serialize function

--- src/misc/serializeConfig.ts
+++ src/misc/serializeConfig.ts
@@ -1,11 +1,16 @@
 import { GameSettings } from "components/Game/types";
 import { TargetSegments } from "services/segments";
 
-type Output = {
+type SerializedConfig = string;
+
+type DeserializedConfig = {
   settings: GameSettings;
   targetSegments: TargetSegments;
 };
-type DeserializeConfig = (serializedConfig: string) => Output | undefined;
+
+type DeserializeConfig = (
+  serializedConfig: SerializedConfig
+) => DeserializedConfig | undefined;
 
 export const deserializeConfig: DeserializeConfig = (serializedConfig) => {
   try {
@@ -21,3 +26,10 @@ export const deserializeConfig: DeserializeConfig = (serializedConfig) => {
     return;
   }
 };
+
+type SerializeConfig = (
+  deserializedConfig: DeserializedConfig
+) => SerializedConfig;
+
+export const serializeConfig: SerializeConfig = (params) =>
+  window.btoa(JSON.stringify(params));
