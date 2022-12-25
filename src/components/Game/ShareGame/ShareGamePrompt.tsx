bb9f1750a6eb755955d2fe838f25991dcfbc75cb
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-12-25 13:25:53 +1100

    Add snackbar

--- src/components/Game/ShareGame/ShareGamePrompt.tsx
+++ src/components/Game/ShareGame/ShareGamePrompt.tsx
@@ -1,8 +1,12 @@
 import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 import { TargetSegments } from "services/segments";
 import { GameSettings } from "components/Game/types";
 import { makeSharedGameUrl } from "components/Game/ShareGame/utils";
+import { useState } from "react";
 
 type Props = {
   settings: GameSettings;
@@ -10,19 +14,46 @@ type Props = {
 };
 
 const ShareGamePrompt = (props: Props) => {
+  const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
+
+  const closeSnackbar = () => setIsSnackbarOpen(false);
+
+  const action = (
+    <IconButton
+      size="small"
+      aria-label="close"
+      color="inherit"
+      onClick={closeSnackbar}
+    >
+      <CloseIcon fontSize="small" />
+    </IconButton>
+  );
+
   const url = makeSharedGameUrl(props);
 
-  const handleClick = () => navigator.clipboard.writeText(url);
+  const handleClick = () => {
+    navigator.clipboard.writeText(url);
+    setIsSnackbarOpen(true);
+  };
 
   return (
-    <Button
-      aria-label="new game"
-      fullWidth
-      onClick={handleClick}
-      variant="text"
-    >
-      share game
-    </Button>
+    <>
+      <Button
+        aria-label="new game"
+        fullWidth
+        onClick={handleClick}
+        variant="text"
+      >
+        share game
+      </Button>
+      <Snackbar
+        open={isSnackbarOpen}
+        autoHideDuration={3000}
+        message="Link copied to clipboard"
+        action={action}
+        onClose={closeSnackbar}
+      />
+    </>
   );
 };
 
