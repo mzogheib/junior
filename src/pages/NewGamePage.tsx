52f77e94650eadb4317731f44043ae09d05810f7
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-12-29 20:46:05 +1100

    Fix component name

--- src/pages/NewGamePage.tsx
+++ src/pages/NewGamePage.tsx
@@ -1,10 +1,10 @@
 import NewGameDialog from "components/Game/NewGame/NewGameDialog";
 import PageWrapper from "pages/PageWrapper";
 
-const App = () => (
+const NewGamePage = () => (
   <PageWrapper>
     <NewGameDialog />
   </PageWrapper>
 );
 
-export default App;
+export default NewGamePage;
