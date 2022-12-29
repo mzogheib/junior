09a121d8d59b2d7f3d7f55f52ff485074069d026
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-12-29 22:09:08 +1100

    Add prompts

--- src/pages/SharedGamePage.tsx
+++ src/pages/SharedGamePage.tsx
@@ -1,10 +1,29 @@
 import { useNavigate } from "react-router-dom";
+import styled from "@emotion/styled";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 import { useSharedGame } from "components/Game/ShareGame/utils";
 import { useNewGame } from "components/Game/NewGame/NewGameProvider";
 import PageWrapper from "pages/PageWrapper";
 import { paths } from "pages/PageRouter";
+import Attempt from "components/Game/Attempt";
+import { spacing } from "components/Theme/utils";
+import { GameMode } from "components/Game/types";
+import { SegmentType } from "services/segments";
+
+const Wrapper = styled.div`
+  padding: ${spacing(2)};
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const gameModeMap = {
+  [GameMode.Letters]: "word",
+  [GameMode.Numbers]: "equation",
+};
 
 const SharedGamePage = () => {
   const navigate = useNavigate();
@@ -19,12 +38,57 @@ const SharedGamePage = () => {
     navigate(paths.game);
   };
 
+  if (!initialConfig) {
+    return null;
+  }
+
+  console.log(initialConfig);
+
+  const { mode, targetSegments } = initialConfig;
+
+  const gameMode = gameModeMap[mode];
+
+  const target = targetSegments.map((segment) => segment.value).join("");
+
+  const readOnlyValues = targetSegments
+    .filter((segment) => segment.type === SegmentType.ReadOnly)
+    .map((segment) => segment.value);
+
+  const attempt = targetSegments
+    .map((segment) => {
+      if (segment.type === SegmentType.Writeable) {
+        const newValue = segment.value
+          .split("")
+          .map((_) => "?")
+          .join("");
+        return { ...segment, value: newValue };
+      }
+      return segment;
+    })
+    .map((segment) => segment.value)
+    .join("");
+
   return (
     <PageWrapper>
-      <div>{initialConfig && JSON.stringify(initialConfig)}</div>
-      <Button variant="contained" onClick={handleClick}>
-        Go
-      </Button>
+      <Wrapper>
+        <Attempt
+          target={target}
+          attempt={attempt}
+          readOnlyValues={readOnlyValues}
+        />
+
+        <br />
+
+        <Typography variant="body1">You've been challenged!</Typography>
+
+        <Typography variant="body1">Can you solve this {gameMode}?</Typography>
+
+        <br />
+
+        <Button variant="contained" onClick={handleClick}>
+          Go
+        </Button>
+      </Wrapper>
     </PageWrapper>
   );
 };
