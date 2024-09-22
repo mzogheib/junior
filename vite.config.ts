d4033fd3a00557c448d858f3b187d892ad913a54
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-09-22 10:10:20 +1000

    Update path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,11 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      components: path.resolve(__dirname, "./src/components"),
+      misc: path.resolve(__dirname, "./src/misc"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      services: path.resolve(__dirname, "./src/services"),
+      sharedState: path.resolve(__dirname, "./src/sharedState"),
     },
   },
 });
