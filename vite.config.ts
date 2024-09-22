251b24bd979814376437849c61f4a4da33990237
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-09-22 16:36:31 +1000

    Manually set paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   root: "src",
   build: {
     emptyOutDir: true,
     outDir: "../dist",
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, "./src/components"),
+      misc: path.resolve(__dirname, "./src/misc"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      services: path.resolve(__dirname, "./src/services"),
+      sharedState: path.resolve(__dirname, "./src/sharedState"),
+    },
   },
 });
