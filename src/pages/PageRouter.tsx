8bd70714f80ba876b5561fe1bc37d8cb6d142822
Marwan Zogheib <mzogheib@users.noreply.github.com>
2025-01-27 17:01:16 +1100

    fix: go back to hash router (#10)

--- src/pages/PageRouter.tsx
+++ src/pages/PageRouter.tsx
@@ -1,14 +1,9 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
+import { createHashRouter, RouterProvider, Navigate } from "react-router-dom";
 
 import NewGamePage from "pages/NewGamePage";
 import GamePage from "pages/GamePage";
 import GameSuccessPage from "pages/GameSuccessPage";
 import CustomGamePage from "pages/CustomGamePage";
-import { BASE_PATH } from "./config";
 
 export const paths = {
   home: "/",
@@ -21,31 +16,28 @@ type Keys = keyof typeof paths;
 
 export type Paths = typeof paths[Keys];
 
-const router = createBrowserRouter(
-  [
-    {
-      path: paths.home,
-      element: <NewGamePage />,
-    },
-    {
-      path: paths.game,
-      element: <GamePage />,
-    },
-    {
-      path: paths.gameSuccess,
-      element: <GameSuccessPage />,
-    },
-    {
-      path: paths.customGame,
-      element: <CustomGamePage />,
-    },
-    {
-      path: "*",
-      element: <Navigate to={paths.home} />,
-    },
-  ],
-  { basename: BASE_PATH }
-);
+const router = createHashRouter([
+  {
+    path: paths.home,
+    element: <NewGamePage />,
+  },
+  {
+    path: paths.game,
+    element: <GamePage />,
+  },
+  {
+    path: paths.gameSuccess,
+    element: <GameSuccessPage />,
+  },
+  {
+    path: paths.customGame,
+    element: <CustomGamePage />,
+  },
+  {
+    path: "*",
+    element: <Navigate to={paths.home} />,
+  },
+]);
 
 const PageRouter = () => <RouterProvider router={router} />;
 
