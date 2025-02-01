93a604434567d815ae246d4847dd96a52200f712
Marwan Zogheib <mzogheib@users.noreply.github.com>
2025-02-01 16:24:15 +1100

    Overhail everything based on vite PWA guide

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-
-import reportWebVitals from "./reportWebVitals";
 import ThemeProvider from "components/Theme/ThemeProvider";
 import GlobalStyles from "components/Theme/GlobalStyles";
 import PageRouter from "pages/PageRouter";
@@ -24,13 +21,3 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
   root: "src",
+  base: "/junior/",
   build: {
     emptyOutDir: true,
     outDir: "../dist",
@@ -18,4 +19,50 @@ export default defineConfig({
       sharedState: path.resolve(__dirname, "./src/sharedState"),
     },
   },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        short_name: "Junior",
+        name: "Junior",
+        icons: [
+          {
+            src: "logo192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
 });
