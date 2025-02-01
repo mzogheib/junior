bfe7b6507329deccffcd333084acf4d02810ca4d
Marwan Zogheib <mzogheib@users.noreply.github.com>
2025-02-02 09:08:26 +1100

    build: switch to Vite (#6)

--- src/components/BuildNumber.tsx
+++ src/components/BuildNumber.tsx
@@ -2,8 +2,9 @@ import Typography from "@mui/material/Typography";
 
 const BuildNumber = () => (
   <Typography align="center" color="GrayText">
-    <code>build {process.env.REACT_APP_GIT_SHA}</code>
+    <code>build {import.meta.env["VITE_APP_GIT_SHA"]}</code>
   </Typography>
 );
 
 export default BuildNumber;
+("");
---
+++ vite.config.ts
@@ -0,0 +1,68 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
+
+export default defineConfig({
+  root: "src",
+  base: "/junior/",
+  build: {
+    emptyOutDir: true,
+    outDir: "../build",
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, "./src/components"),
+      misc: path.resolve(__dirname, "./src/misc"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      services: path.resolve(__dirname, "./src/services"),
+      sharedState: path.resolve(__dirname, "./src/sharedState"),
+    },
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        short_name: "Junior",
+        name: "Junior",
+        icons: [
+          {
+            src: "logo192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
